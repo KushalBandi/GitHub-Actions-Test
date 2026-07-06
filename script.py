65f58d28977fb6beab6326603216a994f1fdce40
KushalBandi <38084568+KushalBandi@users.noreply.github.com>
2026-07-05 22:35:15 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import os
 
 def main():
-    param_1 = os.environ.get("PARAM_1")
-    param_2 = os.environ.get("PARAM_2")
-    param_3 = os.environ.get("PARAM_3")
+    param_1 = os.environ.get("TEST_PARAM_1")
+    param_2 = os.environ.get("TEST_PARAM_2")
+    param_3 = os.environ.get("TEST_PARAM_3")
 
     print(f"param_1: {param_1}")
     print(f"param_2: {param_2}")
